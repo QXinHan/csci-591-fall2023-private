fe97f74427a7a38dbf6ec80208bf40c2d50d64aa
QXinHan <128396319+QXinHan@users.noreply.github.com>
2023-08-15 06:17:29 +0800

    Update program2.cpp

--- project3/sourcecodex86/program2.cpp
+++ project3/sourcecodex86/program2.cpp
@@ -4,9 +4,9 @@
 #include <ntstatus.h>
 typedef NTSTATUS(WINAPI* fnNtUnmapViewOfSection)(HANDLE ProcessHandle, PVOID BaseAddress);
 // Rva to Foa
-DWORD RVATOFOA(DWORD file_buffer, DWORD Rva) {
+DWORD RVATOFOA(PVOID file_buffer, DWORD Rva) {
 	PIMAGE_DOS_HEADER pdos = (PIMAGE_DOS_HEADER)file_buffer;
-	PIMAGE_NT_HEADERS pnt = (PIMAGE_NT_HEADERS)(pdos->e_lfanew + file_buffer);
+	PIMAGE_NT_HEADERS pnt = (PIMAGE_NT_HEADERS)(pdos->e_lfanew + (DWORD)file_buffer);
 	PIMAGE_SECTION_HEADER psec = (PIMAGE_SECTION_HEADER)(pnt + 1);
 	if (Rva <= pnt->OptionalHeader.SizeOfHeaders) 
 	{
@@ -25,11 +25,37 @@ DWORD RVATOFOA(DWORD file_buffer, DWORD Rva) {
 	printf("failed RVA to FOA\n");
 	return -1;
 }
+DWORD repairRelocationTable(PVOID peFile_exe1,DWORD ImageOffset) {
+	PIMAGE_DOS_HEADER pDos = NULL;
+	PIMAGE_NT_HEADERS pNt = NULL;
+	PIMAGE_BASE_RELOCATION pBaserel = NULL;
+	pDos = (PIMAGE_DOS_HEADER)peFile_exe1;
+	pNt = (PIMAGE_NT_HEADERS)(((DWORD)peFile_exe1 + pDos->e_lfanew));
+	pNt->OptionalHeader.ImageBase = pNt->OptionalHeader.ImageBase + ImageOffset;//This code is important , without it the program1.exe will not excute.
+	pBaserel = (PIMAGE_BASE_RELOCATION)((DWORD)peFile_exe1 + RVATOFOA(peFile_exe1, pNt->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_BASERELOC].VirtualAddress));
+	while (pBaserel->VirtualAddress && pBaserel->SizeOfBlock) {
+		DWORD NumberOfItems = (pBaserel->SizeOfBlock - 8) / 2;
+		PWORD data = (PWORD)(((DWORD)pBaserel) + 8);
+		for (size_t num = 0; num < NumberOfItems; num++) 
+		{
+			if (((*data) & 0xf000) == 0x3000) {
+				DWORD changeAddr = (*data) & 0x0fff;
+				changeAddr = pBaserel->VirtualAddress + changeAddr; 
+				changeAddr = (DWORD)peFile_exe1 + RVATOFOA(peFile_exe1, changeAddr);
+				*((PDWORD)changeAddr) += ImageOffset;
+			}
+			data++;
+		}
+		pBaserel = (PIMAGE_BASE_RELOCATION)(pBaserel->SizeOfBlock + (DWORD)pBaserel);
+	}
+
+	return 0;
+}
 int main() {
 	printf("Hello program2\n");
-	LPVOID image1;
+	PVOID image1;
 	char* image2;
-	LPVOID peFile1;
+	PVOID peFile_exe1;
 	// To get the program2.exe file
 	image2 = (char*)GetModuleHandle(NULL);
 	if (image2 == NULL) {
@@ -39,15 +65,15 @@ int main() {
 	// Parse the program2.exe PE format and decrypt the section ".shell" to get program1.exe 
 	PIMAGE_DOS_HEADER pDos = (PIMAGE_DOS_HEADER)image2;
 	PIMAGE_NT_HEADERS pNt = (PIMAGE_NT_HEADERS)(pDos->e_lfanew + image2);
-	SIZE_T OEP_exe2 = pNt->OptionalHeader.AddressOfEntryPoint;
-	SIZE_T imageBase_exe2 = pNt->OptionalHeader.ImageBase;
+	DWORD OEP_exe2 = pNt->OptionalHeader.AddressOfEntryPoint;
+	DWORD imageBase_exe2 = pNt->OptionalHeader.ImageBase;
 	PIMAGE_SECTION_HEADER pSec = (PIMAGE_SECTION_HEADER)(pNt + 1);
-	SIZE_T secCnt = pNt->FileHeader.NumberOfSections;
-	SIZE_T textStart;
-	SIZE_T textSize;
+	DWORD secCnt = pNt->FileHeader.NumberOfSections;
+	DWORD textStart;
+	DWORD textSize;
 	while (1) {
 		if (!memcmp(pSec->Name, ".shell", 7)) {
-			textStart = pSec->VirtualAddress + (SIZE_T)image2;
+			textStart = pSec->VirtualAddress + (DWORD)image2;
 			textSize = pSec->SizeOfRawData;
 			break;
 		}
@@ -60,20 +86,20 @@ int main() {
 	pDos = (PIMAGE_DOS_HEADER)textStart;
 	pNt = (PIMAGE_NT_HEADERS)(pDos->e_lfanew + textStart);
 	pSec = (PIMAGE_SECTION_HEADER)(pNt + 1);
-	SIZE_T imageSize_exe1 = pNt->OptionalHeader.SizeOfImage;
-	SIZE_T imageHeaderSize_exe1 = pNt->OptionalHeader.SizeOfHeaders;
-	SIZE_T imageBase_exe1 = pNt->OptionalHeader.ImageBase;
-	SIZE_T imageOEP_exe1 = pNt->OptionalHeader.AddressOfEntryPoint;
-	SIZE_T cntSec = pNt->FileHeader.NumberOfSections;
+	DWORD imageSize_exe1 = pNt->OptionalHeader.SizeOfImage;
+	DWORD imageHeaderSize_exe1 = pNt->OptionalHeader.SizeOfHeaders;
+	DWORD imageBase_exe1 = pNt->OptionalHeader.ImageBase;
+	DWORD imageOEP_exe1 = pNt->OptionalHeader.AddressOfEntryPoint;
+	DWORD cntSec = pNt->FileHeader.NumberOfSections;
 	//Stretch the program1.exe to the memory
-	peFile1 = (LPVOID)malloc(textSize);
-	memset(peFile1, 0, textSize);
-	memcpy(peFile1, (void*)textStart, textSize);
+	peFile_exe1 = (LPVOID)malloc(textSize);
+	memset(peFile_exe1, 0, textSize);
+	memcpy(peFile_exe1, (void*)textStart, textSize);
 	image1 = (LPVOID)malloc(imageSize_exe1);
 	memset(image1, 0, imageSize_exe1);
-	memcpy(image1, (void*)peFile1, imageHeaderSize_exe1);
-	for (SIZE_T i = 0; i < cntSec; i++) {
-		memcpy((LPVOID)((SIZE_T)image1 + pSec[i].VirtualAddress), (LPVOID)((SIZE_T)peFile1 + pSec[i].PointerToRawData), pSec[i].SizeOfRawData);
+	memcpy(image1, (void*)peFile_exe1, imageHeaderSize_exe1);
+	for (DWORD i = 0; i < cntSec; i++) {
+		memcpy((LPVOID)((DWORD)image1 + pSec[i].VirtualAddress), (LPVOID)((DWORD)peFile_exe1 + pSec[i].PointerToRawData), pSec[i].SizeOfRawData);
 	}
 	// Create a suspended process "program2.exe"
 	TCHAR appName[] = TEXT("program2.exe");
@@ -95,7 +121,7 @@ int main() {
 		&pi                       // 进程信息
 	);
 	if (sucess == FALSE) {
-		printf("NONONONO\n");
+		printf("failed to create new process\n");
 		system("pause");
 		return 1;
 	}
@@ -115,14 +141,14 @@ int main() {
 		return 1;
 	}
 	// Use API NtUnmapViewOfSection to uninstall the memory of main thread
-	SIZE_T susProOEP = context.Eip;
-	SIZE_T susProImageBase = context.Ebx + 8;
+	DWORD susProOEP = context.Eip;
+	DWORD susProImageBase = context.Ebx + 8;
 	NTSTATUS status = fNtUnmapViewOfSection(pi.hProcess, (PVOID)susProImageBase);
 	// Use API "VirtualAllocEx" to allocate memory for the stretched program1.exe
-	LPVOID realImage_exe1 = VirtualAllocEx(pi.hProcess, (LPVOID)imageBase_exe1, imageSize_exe1, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+	DWORD realImage_exe1 = (DWORD)VirtualAllocEx(pi.hProcess, (LPVOID)imageBase_exe1, imageSize_exe1, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
 	// Get the expected memory
-	if (realImage_exe1 == (LPVOID)imageBase_exe1) {
-		bool copySuc = WriteProcessMemory(pi.hProcess, realImage_exe1, (LPCVOID)image1, imageSize_exe1, NULL);
+	if (realImage_exe1 == imageBase_exe1) {
+		bool copySuc = WriteProcessMemory(pi.hProcess, (LPVOID)realImage_exe1, (LPCVOID)image1, imageSize_exe1, NULL);
 		if (copySuc == FALSE) {
 			printf("Error:it can not be write in there\nI do not know how to solve it\n");
 			FreeLibrary(hNtdll);
@@ -131,53 +157,32 @@ int main() {
 	}
 	// Not get the expected memory
 	else {
-		printf("Now I will repair this relocation!\n");//for text
 		if (realImage_exe1 == NULL) 
 		{
-			realImage_exe1 = VirtualAllocEx(pi.hProcess, NULL, imageSize_exe1, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
+			realImage_exe1 = (DWORD)VirtualAllocEx(pi.hProcess, NULL, imageSize_exe1, MEM_COMMIT | MEM_RESERVE, PAGE_EXECUTE_READWRITE);
 		}
 		// Repair the relocation table
-		pDos = (PIMAGE_DOS_HEADER)peFile1;
-		pNt = (PIMAGE_NT_HEADERS)(pDos->e_lfanew + (SIZE_T)peFile1);
-		BYTE* relAddr = (BYTE*)(RVATOFOA((SIZE_T)peFile1,pNt->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_BASERELOC].VirtualAddress) + (SIZE_T)peFile1);
-		DWORD relSize = pNt->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_BASERELOC].Size;
-		while (relSize) 
-		{
-			PIMAGE_BASE_RELOCATION pBaserel = (PIMAGE_BASE_RELOCATION)relAddr;
-			DWORD sizeOfBlock = pBaserel->SizeOfBlock;
-			DWORD numOfblock = (sizeOfBlock - 8) / 2;
-			PUSHORT data = (PUSHORT)pBaserel;
-			for (SIZE_T i = 4; i < numOfblock + 4; i++) 
-			{
-				SIZE_T d = RVATOFOA((DWORD)peFile1, pBaserel ->VirtualAddress+(data[i] & 0x0fff))+ (SIZE_T)peFile1;
-				SIZE_T Va = *(SIZE_T*)d + (SIZE_T)realImage_exe1 - imageBase_exe1;
-				if ((data[i] & 0xf000)==0x3000) {
-					*(SIZE_T*)d = Va;
-				}
-			}
-			relSize -= sizeOfBlock;
-			relAddr += sizeOfBlock;
-		}
+		repairRelocationTable(peFile_exe1, (SIZE_T)realImage_exe1 - imageBase_exe1);
 		// Recopy the repaired program1.exe to the buffer "image1"
 		memset(image1, 0, imageSize_exe1);
-		memcpy(image1, (void*)peFile1, imageHeaderSize_exe1);
-		for (SIZE_T i = 0; i < cntSec; i++) {
-			memcpy((LPVOID)((SIZE_T)image1 + pSec[i].VirtualAddress), (LPVOID)((SIZE_T)peFile1 + pSec[i].PointerToRawData), pSec[i].SizeOfRawData);
+		memcpy(image1, (void*)peFile_exe1, imageHeaderSize_exe1);
+		for (DWORD i = 0; i < cntSec; i++) {
+			memcpy((LPVOID)((DWORD)image1 + pSec[i].VirtualAddress), (LPVOID)((DWORD)peFile_exe1 + pSec[i].PointerToRawData), pSec[i].SizeOfRawData);
 		}
-		bool copySuc = WriteProcessMemory(pi.hProcess, realImage_exe1, (LPCVOID)image1, imageSize_exe1, NULL);
+		bool copySuc = WriteProcessMemory(pi.hProcess, (LPVOID)realImage_exe1, (LPCVOID)image1, imageSize_exe1, NULL);
 		if (!copySuc) {
 			printf("Error:i do not know how to solve it!");
 			return 1;
 		}
 	}
 	// Set the context and back to main thread
-	context.Eip = imageOEP_exe1 + (SIZE_T)realImage_exe1;
+	context.Eip = imageOEP_exe1 + (DWORD)realImage_exe1;
 	WriteProcessMemory(pi.hProcess, (LPVOID)(context.Ebx + 8), &realImage_exe1, 4, NULL);
 	context.ContextFlags = CONTEXT_FULL;
 	SetThreadContext(pi.hThread, &context);
 	ResumeThread(pi.hThread);
 	free(image1);
-	free(peFile1);
+	free(peFile_exe1);
 	FreeLibrary(hNtdll);
 	system("pause");
 	return 0;
