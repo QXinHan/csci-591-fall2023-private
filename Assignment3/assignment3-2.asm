4ae6ce10afe1023587788c885b011d55daf36a6e
GitHub 3290862073@qq.com <qianzhuoyun@nanu.edu.cn>
2023-11-27 14:33:39 +0800

    Update assignment3-2.asm
    add some comments in the assignment3-2.asm.

--- Assignment3/assignment3-2.asm
+++ Assignment3/assignment3-2.asm
@@ -6,27 +6,28 @@ ch_out db "%c ",0
 
 section .bss
 temp resq 1
+
 section .text
 global main
 extern printf
 extern ExitProcess
 
-
+;this function is used to print the set of ascii
 function:
     push rbp
     mov rbp,rsp
     sub rsp,32
 
     mov rcx,126
-   
+   ;the decimal value from 32 to 126 can display on the screen
 loop_start:
     mov [temp],rcx
     push rcx
-
+    ;print the ascii by "%c"
     mov rdx,qword [temp]
     mov rcx,ch_out
     call printf
-
+    ;if the rcx is 32 then end the loop and the function
     cmp qword [temp],32
     je func_end
 
@@ -34,9 +35,8 @@ loop_start:
 
 loop loop_start
 
-jmp func_end
-
 func_end:
+    xor rax,rax
     ret
 
 main:
@@ -44,7 +44,9 @@ main:
     mov rbp,rsp
     sub rsp,32
 
+    ;call the print function
     call function
-
+    
+    ;the program exit
     xor rax,rax
     call ExitProcess
