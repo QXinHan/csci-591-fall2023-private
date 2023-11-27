bfcdb9ed8608f79cd14a2726dae5aafb6530e365
GitHub 3290862073@qq.com <qianzhuoyun@nanu.edu.cn>
2023-11-27 11:10:40 +0800

    Update assignment3-2.asm
    it can not jump out the loop

--- Assignment3/assignment3-2.asm
+++ Assignment3/assignment3-2.asm
@@ -4,32 +4,37 @@ default rel
 section .data
 ch_out db "%c ",0
 
-
+section .bss
+temp resq 1
 section .text
 global main
 extern printf
 extern ExitProcess
+
 
 function:
     push rbp
     mov rbp,rsp
     sub rsp,32
 
-    mov rcx,32
+    mov rcx,126
+   
 loop_start:
+    mov [temp],rcx
     push rcx
 
-    mov rdx,rcx
-    lea rcx,[ch_out]
+    mov rdx,qword [temp]
+    mov rcx,ch_out
     call printf
 
-    pop rcx
-    add rcx
-
-    cmp rcx,127
+    cmp qword [temp],32
     je func_end
 
+    pop rcx
+
 loop loop_start
+
+jmp func_end
 
 func_end:
     ret
